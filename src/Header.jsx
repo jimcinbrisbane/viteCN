6f5167401b87d7e08e07b0abf39c232002ee6108
atom1108 <102128680+atom1108@users.noreply.github.com>
2023-03-24 13:02:54 +1000

    Improve Header and Navbar responsiveness

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,7 +10,7 @@ function Header()
             <div id="header-content">
                 <div class="contentwrapper" style={{ backgroundImage: `url(${bg})`, backgroundSize: "20%", backgroundRepeat: 'no-repeat', backgroundPosition: 'right bottom' }}>
                     <div className='edges'>
-                        <img src="./logo.png" alt="Code Network logo" style={{ width: "35%" }} />
+                        <img src="./logo.png" alt="Code Network logo" />
                         <h1>Join the community</h1>
                         <a href="https://qutcode.getqpay.com/" id="action">Sign up  <i class="fa fa-external-link" aria-hidden="true"></i></a>
                         <p> </p>
