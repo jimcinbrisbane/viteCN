11df31cf9d9dda8356281efbedc02758b1676a4d
jimcinbrisbane <jchi@cybermetrix.com.au>
2023-05-31 15:24:36 +1000

    move to hash router so gh-pages support routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import Footer from './Footer'
 import { Routes, Route } from 'react-router-dom';
@@ -10,11 +10,11 @@ import Navbar from './component/Navbar'
 import AnimatedCursor from "./component/cursor"
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter >
     <Navbar />
     <App />
     <Footer />
-  </BrowserRouter>,
+  </HashRouter >,
 
   document.getElementById("root")
 );
