e44541c29a84c295d9c335f8b0a454aa637b27f6
jimcinbrisbane <jchi@cybermetrix.com.au>
2023-08-16 15:02:14 +1000

    add sponsor

--- src/Sponsor.jsx
+++ src/Sponsor.jsx
@@ -4,30 +4,38 @@ import './sponsor.css';
 
 function Sponsor()
 {
-    return (
-        <div id="global-padding">
-          <h1>Our Sponsors:</h1>
-          <h2>Platinum Sponsors</h2>
-            <div className='sponsor-grid'>
-              <a href="https://technologyonecorp.com/" id="action">
-                  <img src="/sponsors/technologyone.png" alt="Sign up" className="button-image" />
-              </a>
-            </div>
-          <h2>Gold Sponsors</h2>
-            <div className='sponsor-grid'>
-              <a href="https://technologyonecorp.com/" id="action">
-                  <img src="/sponsors/atlassian.png" alt="Sign up" className="button-image" />
-              </a>
-              <a href="https://technologyonecorp.com/" id="action">
-                  <img src="/sponsors/macquarie.png" alt="Sign up" className="button-image" />
-              </a>
-              <a href="https://technologyonecorp.com/" id="action">
-                  <img src="/sponsors/riotinto.png" alt="Sign up" className="button-image" />
-              </a>
-          </div>
-        </div>
-      );
-      
+  return (
+    <div id="global-padding">
+      <h1>Our Sponsors:</h1>
+      <h2>Platinum Sponsors</h2>
+      <div className='sponsor-grid'>
+        <a href="https://technologyonecorp.com/" id="action">
+          <img src="/sponsors/technologyone.png" alt="Sign up" className="button-image" />
+        </a>
+      </div>
+      <h2>Gold Sponsors</h2>
+      <div className='sponsor-grid'>
+        <a href="https://www.tanda.co/" id="action">
+          <img src="/sponsors/tanda.png" alt="tanda" className="button-image" />
+        </a>
+        <a href="https://www.atlassian.com/" id="action">
+          <img src="/sponsors/atlassian.png" alt="atlassian" className="button-image" />
+        </a>
+
+        <a href="https://www.riotinto.com/" id="action">
+          <img src="/sponsors/riotinto.png" alt="riotinto" className="button-image" />
+        </a>
+        <a href="https://www2.deloitte.com/us/en/pages/consulting/solutions/cloud-consulting-services.html" id="action">
+          <img src="/sponsors/d.png" alt="deloitte_cloud" className="button-image" />
+        </a>
+        <a href="https://www.tanda.co/" id="action">
+          <img src="/sponsors/macquarie.png" alt="macquarie" className="button-image" />
+        </a>
+      </div>
+
+    </div>
+  );
+
 }
 
 export default Sponsor
