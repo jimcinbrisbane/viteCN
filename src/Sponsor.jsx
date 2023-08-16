e5e56bf00935de8feeeaf54ad056268824087403
jimcinbrisbane <jchi@cybermetrix.com.au>
2023-08-16 15:09:49 +1000

    fix css

--- src/Sponsor.jsx
+++ src/Sponsor.jsx
@@ -16,20 +16,19 @@ function Sponsor()
       <h2>Gold Sponsors</h2>
       <div className='sponsor-grid'>
         <a href="https://www.tanda.co/" id="action">
+          <p> </p>
           <img src="/sponsors/tanda.png" alt="tanda" className="button-image" />
         </a>
+
         <a href="https://www.atlassian.com/" id="action">
           <img src="/sponsors/atlassian.png" alt="atlassian" className="button-image" />
         </a>
-
         <a href="https://www.riotinto.com/" id="action">
           <img src="/sponsors/riotinto.png" alt="riotinto" className="button-image" />
         </a>
         <a href="https://www2.deloitte.com/us/en/pages/consulting/solutions/cloud-consulting-services.html" id="action">
-          <img src="/sponsors/d.png" alt="deloitte_cloud" className="button-image" />
-        </a>
-        <a href="https://www.tanda.co/" id="action">
-          <img src="/sponsors/macquarie.png" alt="macquarie" className="button-image" />
+          <p> </p>
+          <img src="/sponsors/d.jpg" alt="deloitte_cloud" className="button-image" />
         </a>
       </div>
 
