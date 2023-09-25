57cbfe8c9c6922a53e4dd2ea1a420850924a2dba
Corey McCormick <coreymccor@live.com.au>
2023-09-25 18:44:41 +1000

    Making the page look a bit nicer

--- src/Agm.jsx
+++ src/Agm.jsx
@@ -3,38 +3,23 @@ import Rules from './Rules'
 import { Routes, Route } from "react-router-dom"
 import AnimatedCursor from "./component/cursor"
 import teamData from "./component/TeamData.json"
-import './team.css';
+import './agm.css';
 
 function Agm() {
   return (
     <div className="content-wrapper">
       <div className="edges" style={{ color: "#111", fontFamily: 'Montserrat' }}>
         <div className="global-padding">
-          <h1>Code Network will be holding the 2023 AGM on Thursday, 5th October 2023.</h1>
+          <h1>Code Network's 2023 AGM</h1>
           <p>We will be electing our 2024 Executive Team and voting on other business, including constitution amendments.</p>
+          <p><b>When and Where:</b> 5-8pm Thursday 5/10/2023 at QUT GP-D106</p>
           <h2>Key Links</h2>
-          <table>
-            <tr>
-              <th>Document</th>
-              <th>Link</th>
-            </tr>
-            <tr>
-              <td>Agenda</td>
-              <td><a class="link" href="https://codenetwork.co/agmagenda" target="_blank">https://codenetwork.co/agmagenda</a></td>
-            </tr>
-            <tr>
-              <td>Executive Team Positions</td>
-              <td><a class="link" href="https://codenetwork.co/agmpositions" target="_blank">https://codenetwork.co/agmpositions</a></td>
-            </tr>
-            <tr>
-              <td>Executive Team EOI Form</td>
-              <td><a class="link" href="https://codenetwork.co/agmeoi" target="_blank">https://codenetwork.co/agmeoi</a></td>
-            </tr>
-            <tr>
-              <td>Proposed Constitution Amendments</td>
-              <td><a class="link" href="https://codenetwork.co/agmconstitution" target="_blank">https://codenetwork.co/agmconstitution</a></td>
-            </tr>
-          </table>
+          <div style={{ display: 'flex', flexDirection: 'row', gap: '4px', flexWrap: 'wrap' }} id="agmLinks">
+            <a href="https://codenetwork.co/agmagenda" target="_blank">Agenda</a>
+            <a href="https://codenetwork.co/agmpositions" target="_blank">Executive Positions</a>
+            <a href="https://codenetwork.co/agmeoi" target="_blank">Executive EOI</a>
+            <a href="https://codenetwork.co/agmconstitution" target="_blank">Constitution Amendments</a>
+          </div>
         </div>
       </div >
     </div>
