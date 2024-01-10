b6a1f8803671ac39f70068042c79b5a4ee03c387
Patrick <42367024+Patadam@users.noreply.github.com>
2024-01-10 10:42:46 +1000

    create new page for special general meeting

---
+++ src/Sgm.jsx
@@ -0,0 +1,25 @@
+export default function SGM() {
+  return (
+    <div class="edges" style={{ display: "grid" }}>
+      <div style={{ maxWidth: "90ch", placeSelf: "center" }}>
+        <h1>January 2024 Special General Meeting</h1>
+        <p>Dear Code Network members,</p>
+        <p>
+          Due to a significant change in the executive team for 2024 we are required under section 15.2 of the Constitution to call a
+          General Meeting (GM) to elect a replacement to this role.
+        </p>
+        <p>
+          We are currenting communicating with the Guild to confirm details around this GM. We would appreciate your attendance and support
+          in this matter and will be able to answer questions during the meeting.
+        </p>
+        <p>
+          We are seeking any members interested in taking on the responsibilities and roles of <strong>Treasurer</strong> to attend and make
+          their interest known to the President.
+        </p>
+        <div style={{ background: "#eee", padding: "1rem 2rem", borderRadius: "8px", marginTop: "2rem" }}>
+          <p>The date, time and location for this event are being organised, please check back Monday for additional information</p>
+        </div>
+      </div>
+    </div>
+  );
+}
