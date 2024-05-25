c3fe3f25b91612a771d7ed29278646dd9fc5555b
Patrick <42367024+Patadam@users.noreply.github.com>
2024-05-25 17:59:06 +1000

    feat: update hackathon page

--- src/routes/Hackathon.jsx
+++ src/routes/Hackathon.jsx
@@ -21,8 +21,12 @@ export default function HackathonPage() {
                 12-14 July 2024 <span className="px-2">|</span> The Precinct <span className="px-2">|</span> Fortitude Valley
               </p>
               <div className="buttons-list">
-                <a className="btn">Register Today</a>
-                <a className="btn">Facebook Event</a>
+                <a className="btn" href="https://events.humanitix.com/code-network-winter-hackathon-2024">
+                  Register Today
+                </a>
+                <a className="btn" href="https://www.facebook.com/events/742663191123606/">
+                  Facebook Event
+                </a>
               </div>
             </div>
           </div>
@@ -31,10 +35,26 @@ export default function HackathonPage() {
       <div className="edges">
         <div>
           <h2 style={{ marginTop: "0" }}>What on earth is a Hackathon?</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, nobis? Eum optio eligendi nihil maiores quibusdam tempore
-            ipsa sint corporis temporibus, aut animi facilis a ab ipsum doloremque hic deleniti.
-          </p>
+          <div style={{ maxWidth: "80ch" }}>
+            <p>
+              A hackathon is a collaborative event where you can put your IT skills to the test and build innovative projects quickly. It's
+              a great chance to build and present your amazing tech concepts and ideas.
+            </p>
+            <p>
+              In a hackathon you'll usually work in team of similiarly interested IT students and professionals. And you'll have the
+              opportunity to learn new skills and network with other attendees. Oh and don't forget the food! The Code Network hackathon
+              will include food to keep you energised and building.
+            </p>
+            <p>
+              Code Network welcomes everyone interested in particpating to register, there is no minimum required skillset to be involved.
+            </p>
+            <br />
+            <h3>How do I get involved?</h3>
+            <p>
+              Use one of the registration links on this page to purchase a ticket from our event page on Humanitix and just turn up Friday
+              night ready to go! Stay tuned to our socials to more information, ask questions and be notified about any updates.
+            </p>
+          </div>
           {/* <p>
           <b>Top Teams Last Year</b>
         </p>
@@ -51,7 +71,9 @@ export default function HackathonPage() {
         </p>
         <div className="end-cta">
           <p>Ready to just say yes!</p>
-          <a className="btn">Register Today</a>
+          <a className="btn" href="https://events.humanitix.com/code-network-winter-hackathon-2024">
+            Register Today
+          </a>
         </div>
       </div>
     </main>
