4657ff66d34a3e83220d3b72fdbcde79154631b0
estate-market-admin <admin@estatemarket.com.au>
2024-06-15 22:00:38 +1000

    removed dot points added arrows to faq

--- src/components/FAQSection/index.jsx
+++ src/components/FAQSection/index.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import "./index.css";
-import  faqArray from "./faqArray";
+import faqArray from "./faqArray";
+import "@fortawesome/fontawesome-free/css/all.min.css"; // Import Font Awesome CSS
+
 const FAQSection = () => {
   const [openIndex, setOpenIndex] = useState(null);
 
   const toggleFAQ = (index) => {
     setOpenIndex(openIndex === index ? null : index);
   };
-
 
   const faqs = faqArray;
 
@@ -18,22 +19,27 @@ const FAQSection = () => {
           <h2 className="cd-faq__title">Frequently Asked Questions</h2>
           {faqs.map((faq, index) => (
             <li className="cd-faq__item" key={index}>
-              <a className={`cd-faq__trigger ${openIndex === index ? "open" : ""}`} href="#0" onClick={() => toggleFAQ(index)}>
+              <a
+                className={`cd-faq__trigger ${openIndex === index ? "open" : ""}`}
+                href="#0"
+                onClick={() => toggleFAQ(index)}
+              >
                 <span>{faq.question}</span>
+                <i className={`fas fa-chevron-${openIndex === index ? "up" : "down"}`}></i>
               </a>
               <div className={`cd-faq__content ${openIndex === index ? "open" : ""}`}>
                 <div className="text-component">
                   <p>{faq.answer}</p>
                 </div>
-              </div> {/* cd-faq__content */}
+              </div>
             </li>
           ))}
-        </ul> {/* cd-faq__group */}
-      </div> {/* cd-faq__items */}
+        </ul>
+      </div>
 
       <a href="#0" className="cd-faq__close-panel text-replace">Close</a>
       <div className="cd-faq__overlay" aria-hidden="true"></div>
-    </section> /* cd-faq */
+    </section>
   );
 };
 
