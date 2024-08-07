6d07f5799cd88a06b7425a9e1548cf8c7a980218
estate-market-admin <105776319+JesseOSullivan@users.noreply.github.com>
2024-08-07 11:50:51 +1000

    added img

--- src/Sponsor.jsx
+++ src/Sponsor.jsx
@@ -15,10 +15,10 @@ function Sponsor() {
       <h1>Thank you to our 2024 Sponsors!</h1>
       <h2>Platinum Sponsors</h2>
       <div className="sponsor-grid">
-        <a href="https://www.technologyonecorp.com/" id="action">
-          TechnologyOne
-        </a>
-      </div>
+  <a href="https://www.technologyonecorp.com/" id="action">
+    <img src="/sponsors/techOneWhite.svg" alt="TechOne" className="button-image" style={{ width: "200px", height: "auto" }} />
+  </a>
+</div>
       <h2>Gold Sponsors</h2>
       <div className="sponsor-grid">
         <a href="https://www.tanda.co/" id="action">
