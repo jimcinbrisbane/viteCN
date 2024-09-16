71f8dbbe391f29988243d398441ea1842bd211b7
Corey McCormick <coreymccor@live.com.au>
2024-09-16 18:02:21 +1000

    Replace 2023 constitution with 2024 version

--- src/constitution.jsx
+++ src/constitution.jsx
@@ -3,10 +3,10 @@ function Constitution() {
   return (
     <div className="edges">
       <h1>Code Network Constitution</h1>
-      <p>This version was adopted at the 2023 Annual General Meeting of Code Network. Future revisions will appear here.</p>
+      <p>This version was adopted at the 2024 Annual General Meeting of Code Network. Future revisions will appear here.</p>
       <object
         type="application/pdf"
-        data="/constitution_2023.pdf" // be sure to update this
+        data="/constitution_2024.pdf" // be sure to update this
         width="100%"
         height="600"
       />
